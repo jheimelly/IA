b9bbdb68bdc8bec84d6bcaf0816218ca21a24751
jheimelly <167384761+jheimelly@users.noreply.github.com>
2025-05-26 10:10:18 -0300

    ç

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+ANA LIVIA KLEINER <ana.kleiner@escola.pr.gov.br>
+10:07 (há 0 minuto)
+para mim
+
 const caixaPrincipal = document.querySelector("caixa-principal");
 const caixaPerguntas = document.querySelector("caixa-perguntas");
 const caixaAlternativas = document.querySelector("caixa-alternativas");
@@ -6,19 +11,35 @@ const textoResultado = document.querySelector("texto-resultado");
 const perguntas = [
     {
         enunciado: "Pergunta 1",
-        alternativas: ["Alternativa1","Alternativa2"]
+        alternativas: ["Alternativa1
+            {
+            texto:"texto aternativa 1", 
+            afirmação:"afirmacao 1"},
+        
+            {
+            texto:"texto aternativa 2", 
+            afirmação:"afirmacao 2"},
+        ]
     },
     {
-        enunciado: "Pergunta 2",
-        alternativas: ["Alternativa1","Alternativa2"]
+      enunciado: "Pergunta 1",
+        alternativas: ["Alternativa1
+            {
+            texto:"texto aternativa 1", 
+            afirmação:"afirmacao 1"},
+        
+            {
+            texto:"texto aternativa 2", 
+            afirmação:"afirmacao 2"},
+        ]  
     }
 ];
 
 let atual = 0;
 let perguntaAtual;
 
-function mostraPergunta(){
-    perguntaAtual = perguntas[atual];
+function mostrarPergunta(){
+    perguntaAtual = perguntas [atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
-mostraPergunta();
+mostrarPergunta()
